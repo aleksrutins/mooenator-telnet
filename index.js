67e68fcb6f07e072a50dfe33e258d0bacd3f964b
orvismac <orvismac@orvismacs-MacBook-Pro.local>
2018-06-22 13:08:08 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const Telnet = require('ranvier-telnet');
+const server = new Telnet.TelnetServer(rawSocket => {
+  const telnetSocket = new Telnet.TelnetSocket();
+  telnetSocket.attach(rawSocket);
+  telnetSocket.write(`Welcome to my Telnet server!\n
+The prompt is based on the original Apple 1 system monitor prompt, although the commands are nothing like in the system monitor:\n
+moo     help    ?   quit    exit\n`)
+  telnetSocket.write('\\\n')
+  telnetSocket.on('data', data => {
+    switch(data.toString()) {
+        case "moo":
+            telnetSocket.write("Right back at ya!\n");
+            break;
+        case "?":
+        case "help":
+            telnetSocket.write("Type 'moo', 'quit', or 'exit'.\n")
+            break;
+        case "quit":
+        case "exit":
+        telnetSocket.write('Remember to have a healthy moo at 4:20 each day! See ya later, mooenator!');
+            telnetSocket.destroy();
+            return;
+    }
+    telnetSocket.write('\\\n')
+  });
+}).netServer;
+ 
+server.listen(4000);
